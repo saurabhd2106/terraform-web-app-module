40c4ab0aa7047fb3bc4c010d4f4f05cfe39777ab
Saurabh Dhingra <saurabh.d2106@gmail.com>
2021-07-15 19:27:45 +0530

    Update Web App Module

---
+++ app-service.tf
@@ -0,0 +1,13 @@
+resource "azurerm_app_service" "example" {
+  name                = var.appServiceName
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  app_service_plan_id = azurerm_app_service_plan.app-service-plan.id
+
+  site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type                 = "LocalGit"
+  }
+
+ 
+}
